15662d240e6a65a44b086f1b32a38ae5eb9aa33f
leonid <blink_182@inbox.ru>
2022-12-11 22:35:09 +0300

    triumf

---
+++ index.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 3,
+    spaceBetween: 50,
+    loop: true,
+  
+    
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    
+    
+  });
+
