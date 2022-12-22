b06a7fb6d98ebb4948fb8664556395445b1da8a7
unknown <blink_182@inbox.ru>
2022-12-23 02:46:12 +0300

    adaptive main

--- index.js
+++ index.js
@@ -12,7 +12,41 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   
-    
+    breakpoints: {
+      
+     
+      400: {
+        slidesPerView: 1,
+      
+      },
+      600: {
+        slidesPerView: 1,
+      
+      },
+      720: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      800: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      900: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      1060: {
+        slidesPerView: 3,
+        spaceBetween: 10
+      },
+      // when window width is >= 640px
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 50
+      }
+      
+    },
     
   });
 
+  
